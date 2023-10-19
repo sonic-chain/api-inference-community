0fe494842ecd26dc328e08259518f790bf42261c
sonic <longzhang>
2023-10-19 11:45:06 +0800

    add torch

--- docker_images/transformers/app/pipelines/transformers_common.py
+++ docker_images/transformers/app/pipelines/transformers_common.py
@@ -1,3 +1,4 @@
+import torch
 from typing import Any, Dict
 from app.pipelines import Pipeline
 from transformers import pipeline
@@ -9,8 +10,12 @@ class TransformersPipeline(Pipeline):
         self,
         model_id: str,
     ):
+        if torch.cuda.is_available():
+            device = 0
+        else:
+            device = -1
         model_data = model_info(model_id)
-        self.pipeline = pipeline(model=model_data.modelId, task=model_data.pipeline_tag)
+        self.pipeline = pipeline(model=model_data.modelId, task=model_data.pipeline_tag, device=device)
 
     def __call__(self, inputs: Dict[str, str]) -> Dict[str, Any]:
         return self.pipeline(inputs)
